967cb8710066de594a600b78d9f4a74ef4201e92
arensin8 <arensinani4@gmail.com>
2024-05-02 14:07:35 +0400

    both side stream configs

--- client.js
+++ client.js
@@ -2,7 +2,8 @@ const grpc = require("@grpc/grpc-js");
 const grpcLoader = require("@grpc/proto-loader");
 const echoProto = grpcLoader.loadSync("echo.proto", {});
 const echoDefinition = grpc.loadPackageDefinition(echoProto);
-const serverURL = "localhost:5000";
+const serverURL = "localhost:5001"; // Change to an available port
+
 const { echoPackage } = echoDefinition;
 
 const requestMessage = { value: "Hello, gRPC!" };
@@ -12,38 +13,12 @@ const client = new echoPackage.EchoService(
   grpc.credentials.createInsecure()
 );
 
-client.EchoUnary(requestMessage, (error, response) => {
-  if (error) {
-    console.error("Error:", error);
-  } else {
-    console.log("Response:", response);
-  }
-});
 
-const serverStream = client.EchoServerStream();
-serverStream.on("data", (data) => {
-  console.log(data);
-});
-serverStream.on("end", (err) => {
-  console.log("Error:", err);
-});
 
-const echos = [
-  { value: "value1" },
-  { value: "value2" },
-  { value: "value3" },
-  { value: "value4" },
-];
-const clientStream = client.EchoClinetStream();
-
-for (let index = 0; index < echos.length; index++) {
-  clientStream.write(echos[index]);
-}
-
-clientStream.on("data", (data) => {
-  console.log(data);
-});
-
-clientStream.on("end", (err) => {
-  console.log(err);
+const dateTime = client.dateTime();
+setInterval(() => {
+  dateTime.write({ value: new Date().toLocaleString() });
+}, 1000);
+dateTime.on("data", (data) => {
+  console.log("client dateTime :", data);
 });
--- server.js
+++ server.js
@@ -2,39 +2,56 @@ const grpc = require("@grpc/grpc-js");
 const protoLoader = require("@grpc/proto-loader");
 const echoProto = protoLoader.loadSync("echo.proto", {});
 const echoDefinition = grpc.loadPackageDefinition(echoProto);
-const serverURL = "localhost:5000";
+const serverURL = "localhost:5001"; // Change to an available port
+
 const { echoPackage } = echoDefinition;
 const server = new grpc.Server();
 
 function EchoUnary(call, callback) {
-  console.log("call :", call.request);
-  callback(null, { message: "received" });
+  console.log("Call : ", call.request);
+  callback(null, {
+    message: "recived",
+  });
 }
 
-function EchoClinetStream(call, callback) {
+function EchoClientStream(call, callback) {
+  const list = [];
   call.on("data", (data) => {
-    console.log(data);
+    list.push(data);
+    console.log("Server: ", data);
   });
   call.on("end", (err) => {
+    console.log(list);
     console.log(err);
   });
 }
 
 function EchoServerStream(call, callback) {
   for (let index = 0; index < 10; index++) {
-    call.write({ value: index });
-    call.on("end", (err) => {
-      console.log(err);
+    call.write({
+      value: index,
     });
   }
+  call.on("end", (err) => {
+    console.log(err);
+  });
 }
-function EchoBidiStream(call, callback) {}
+
+function dateTime(call, callback) {
+  call.on("data", (data) => {
+    console.log("server dateTime :", data);
+    call.write({ value: new Date().toLocaleString() });
+  });
+  call.on("end", (err) => {
+    console.log(err);
+  });
+}
 
 server.addService(echoPackage.EchoService.service, {
-  EchoUnary,
-  EchoClinetStream,
-  EchoServerStream,
-  EchoBidiStream,
+  // EchoUnary,
+  // EchoClientStream,
+  // EchoServerStream,
+  dateTime,
 });
 
 server.bindAsync(
